be5aa0212db12746697eda47fdd51874b1004f86
Tim Brooks <brooks@cern.ch>
2015-01-15 20:46:59 +0000

    Update Rust syntax

--- points.rs
+++ points.rs
@@ -1,34 +1,36 @@
 #![crate_type = "dylib"]
 #![crate_name = "points"]
+#![allow(unstable)]
 
 // Based on http://blog.skylight.io/bending-the-curve-writing-safe-fast-native-gems-with-rust/
 // by Yehuda Katz
 
 
-use std::num::pow;
+use std::num::{Int, Float};
 
-pub struct Point { x: int, y: int }  
-struct Line  { p1: Point, p2: Point }
+#[derive(Copy)]
+pub struct Point { x: isize, y: isize }
+struct Line { p1: Point, p2: Point }
 
-impl Line {  
+impl Line {
   pub fn length(&self) -> f64 {
     let xdiff = self.p1.x - self.p2.x;
     let ydiff = self.p1.y - self.p2.y;
-    ((pow(xdiff, 2) + pow(ydiff, 2)) as f64).sqrt()
+    ((xdiff.pow(2) + ydiff.pow(2)) as f64).sqrt()
   }
 }
 
 #[no_mangle]
-pub extern "C" fn make_point(x: int, y: int) -> Box<Point> {  
-    box Point { x: x, y: y }
+pub extern "C" fn make_point(x: isize, y: isize) -> Box<Point> {
+    Box::new(Point{x: x, y: y})
 }
 
 #[no_mangle]
-pub extern "C" fn free_point(p: Box<Point>)  {  
+pub extern "C" fn free_point(p: Box<Point>)  {
    drop(p);
 }
 
 #[no_mangle]
-pub extern "C" fn get_distance(p1: &Point, p2: &Point) -> f64 {  
-    Line { p1: *p1, p2: *p2 }.length()
+pub extern "C" fn get_distance(p1: Box<Point>, p2: Box<Point>) -> f64 {
+    Line{p1: *p1, p2: *p2 }.length()
 }
